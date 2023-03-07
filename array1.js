af1ce0bc34c8111a9a28085e0d4a8f28ca8e6d40
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-07 22:12:31 +0800

    first commit

---
+++ array1.js
@@ -0,0 +1,14 @@
+let kotak=[
+     ["susu","keju","kacang","telur","mie"],
+     ["air","kecap","tempe","tahu","kedelai"],
+     ["roti","nasi","beras","gandum","teh"]
+]
+console.log (kotak [0][0])
+console.log (kotak [0][2])
+console.log (kotak [0][4])
+console.log (kotak [1][0])
+console.log (kotak [1][2])
+console.log (kotak [1][4])
+console.log (kotak [2][0])
+console.log (kotak [2][2])
+console.log (kotak [2][4])
