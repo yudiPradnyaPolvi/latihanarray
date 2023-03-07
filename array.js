b482b569702299a4d0eb8573f13ac99870759a30
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-07 21:44:34 +0800

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+let kotak1 = ["susu","keju","kacang","telur","mie"]
+let kotak2 = ["air","kecap","tempe","tahu","kedelai"]
+let kotak3 = ["roti","nasi","beras","gandum","roti"]
+
+
+console.log(kotak1[1])
+console.log(kotak1[3])
+
+console.log(kotak2[1])
+console.log(kotak2[3])
+
+console.log(kotak3[1])
+console.log(kotak3[3])
